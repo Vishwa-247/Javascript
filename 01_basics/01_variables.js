4e5fd29354f51174d47bca37db0cff44eb8313ea
Vishwa Thouti <113163242+Vishwa-247@users.noreply.github.com>
2025-02-18 15:03:59 +0000

    datatypes done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,7 +10,6 @@ accoutEmail="vishwateja.thouti_2026@woxsen.edu.in"
 accountPassword="34567"
 
 console.log(accountPassword);
-
 /* prefer not to use var because of th block scope and functional scope */
 
 console.table([accountId , accoutEmail ,accountPassword ,accountCity , accountState ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we re using nodejs ,not browser 
+
+console.log(
+    3 
+    +
+    3) // code readability should be high
+
+
+let name  = "vishwa"
+let age  = 18 
+let isLoggedIn = false 
+ 
+// number => 2 to power 45
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => no value assigned
+// symbol => use in react // unique
+
+
+//object 
+
+
+console.log(typeof undefined)  //undefined
+
+console.log(typeof null) //object
+
+    
