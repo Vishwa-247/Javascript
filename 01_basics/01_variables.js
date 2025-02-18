1443edbbdf3b2d9d2c9da8bd002215dcc068bd44
Vishwa Thouti <113163242+Vishwa-247@users.noreply.github.com>
2025-02-18 14:08:02 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 23345
+let accoutEmail = "vishwathouti247@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+accoutEmail="vishwateja.thouti_2026@woxsen.edu.in"
+accountPassword="34567"
+
+console.log(accountPassword);
+
+/* prefer not to use var because of th block scope and functional scope */
+
+console.table([accountId , accoutEmail ,accountPassword ,accountCity , accountState ])
